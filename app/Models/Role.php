c834e4493dc6efcf7ee7368639cb243c692d5a23
f3brysan <febrysan1995@gmail.com>
2025-04-02 11:04:48 +0700

    Refactor Permission and Role models to extend Spatie's Permission package and update user migration for UUID primary key

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -2,11 +2,12 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Spatie\Permission\Models\Role as SpatieRole;
 
-class Role extends Model
+class Role extends SpatieRole
 {
     use HasFactory;
     use HasUuids;

b38ea5a8a00109e11510e8d6e365ea15ad41ded3
f3brysan <febrysan1995@gmail.com>
2025-04-02 20:34:04 +0700

    Refactor B_DashboardController index method to include a comment for clarity

--- app/Http/Controllers/Backend/B_DashboardController.php
+++ app/Http/Controllers/Backend/B_DashboardController.php
@@ -6,9 +6,10 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class B_DashboardController extends Controller
-{
-    public function index(){
-        
+{    
+    public function index()
+    {
+        // Load the dashboard view
         return view('back.dashboard.index');
     }
 }

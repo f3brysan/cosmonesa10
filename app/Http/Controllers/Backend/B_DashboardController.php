5abdd9393bdc4f2b037ad80591b116fd06dc6f7d
f3brysan <febrysan1995@gmail.com>
2025-06-12 04:17:40 +0700

    Refactor dashboard view: update layout, enhance income and product sold metrics, and remove unused sections

--- app/Http/Controllers/Backend/B_DashboardController.php
+++ app/Http/Controllers/Backend/B_DashboardController.php
@@ -2,37 +2,55 @@
 
 namespace App\Http\Controllers\Backend;
 
-use App\Http\Controllers\Controller;
+use App\Models\Kiosks;
+use App\Models\Transaction;
 use Illuminate\Http\Request;
+use App\Models\TransactionDetail;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
 
 class B_DashboardController extends Controller
-{        
+{
     public function index()
     {
         // Load the dashboard view
         // Get the user role
         $role = auth()->user()->getRoleNames();
-        
+
         // Switch to the correct role dashboard
         foreach ($role as $key => $value) {
             switch ($value) {
                 case 'superadmin':
                     // Load the superadmin dashboard
-                    return $this->admin_dashboard();                    
+                    return $this->admin_dashboard();
                 case 'pengelola':
                     // Load the pengelola dashboard
-                    return $this->pengelola_dashboard();                    
+                    return $this->pengelola_dashboard();
                 case 'seller':
                     // Load the seller dashboard
-                    return $this->seller_dashboard();                    
+                    return $this->seller_dashboard();
             }
-        }        
+        }
     }
 
-    public function admin_dashboard() 
+    public function admin_dashboard()
     {
-        return view('back.dashboard.index');
-    }
+        $monthNow = date('Y-m');
+        $profit = Transaction::whereIn('payment_status', ['paid', 'success'])
+            ->sum('total');
+        $profitMonth = Transaction::where('created_at', 'like', $monthNow . '%')
+            ->whereIn('payment_status', ['paid', 'success'])
+            ->sum('total');
 
+        $kiosks = Kiosks::all();
+
+        $productSold = DB::table('transaction_details')
+            ->select('transaction_details.*')
+            ->join('transactions', 'transactions.id', '=', 'transaction_details.transaction_id')
+            ->where('transactions.type', '=', 'product')
+            ->whereIn('transactions.payment_status', ['success', 'paid']);                
+        
+        return view('back.dashboard.index', compact('profitMonth', 'profit', 'kiosks', 'productSold'));
+    }
 
 }

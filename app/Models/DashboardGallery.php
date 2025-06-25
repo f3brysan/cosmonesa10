736502dd60baff7cb2bfb6e7243f5064d9342354
Nazarudin Rasid <ranchiddas@live.com>
2025-06-25 21:43:31 +0700

    add dashboard profile and dashboard gallery models

---
+++ app/Models/DashboardGallery.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DashboardGallery extends Model
+{
+    use HasFactory;
+    protected $table = 'dashboard_galley';
+    public $timestamps = false;
+}
